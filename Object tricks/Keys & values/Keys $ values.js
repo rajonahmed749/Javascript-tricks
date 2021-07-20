53e8359cb5c17314dc868368e263a4a73e1f76dd
rajonahmed749 <raazon749@gmail.com>
2021-07-21 00:32:39 +0600

    3 object tricks added

---
+++ Object tricks/Keys & values/Keys $ values.js
@@ -0,0 +1,12 @@
+// Object.key is a object's build in method for getting keys of a object
+
+const items = {
+    rice: "5kg",
+    oil: "10kg",
+    egg: "1 dozen"
+}
+console.log(Object.keys(items)); // Output:- [ 'rice', 'oil', 'egg' ]
+
+
+// getting values of a object
+console.log(Object.values(items)); // Output:- [ '5kg', '10kg', '1 dozen' ]
