aebda3d60a7e34cfd89fc9479eaeb643556de89f
rajonahmed749 <raazon749@gmail.com>
2021-07-21 00:35:25 +0600

    Entries method

---
+++ Object tricks/Entries method/Entries.js
@@ -0,0 +1,8 @@
+//Object.entries() method returns an array 
+const items = {
+    rice: "5kg",
+    oil: "10kg",
+    egg: "1 dozen"
+}
+console.log(Object.entries(items));
+ // Output:- [ [ 'rice', '5kg' ], [ 'oil', '10kg' ], [ 'egg', '1 dozen' ] ]
