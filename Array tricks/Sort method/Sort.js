de64058dc39d0adfd69b56f357aca901baa7262a
rajonahmed749 <raazon749@gmail.com>
2021-07-30 02:09:36 +0600

    one more method added (arry.sort())

--- Array tricks/Sort method/Sort.js
+++ Array tricks/Sort method/Sort.js
@@ -1,15 +1,18 @@
-// const months = ['March', 'Jan', 'Feb', 'Dec'];
-// months.sort();
-// console.log(months);
+const months = ['March', 'Jan', 'Feb', 'Dec'];
+months.sort();
+console.log(months);
 
-// const array1 = [1, 30, 4, 21, 100000];
-// array1.sort();
-// console.log(array1);
+//another example
+const array1 = [1, 30, 4, 21, 100000];
+array1.sort();
+console.log(array1);
 
-// let numbers = [4, 2, 5, 1, 3];
-// numbers.sort((a, b) => a - b);
-// console.log(numbers)
+//example
+let numbers = [4, 2, 5, 1, 3];
+numbers.sort((a, b) => a - b);
+console.log(numbers)
 
+//one more example
 var items = [
     { name: 'Rajon', age: 21 },
     { name: 'Ibrahim', age: 37 },
@@ -23,7 +26,7 @@ var items = [
     return a.age - b.age;
    
   });
-  
+
   console.log(items.sort(function (a, b) {
     return a.age - b.age;
    
