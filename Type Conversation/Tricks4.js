8d0165124be9065aa9404580ad0cd3c5efa5f646
rajonahmed749 <raazon749@gmail.com>
2021-09-07 12:59:31 +0600

    anohter tricks added on Type conversation

---
+++ Type Conversation/Tricks4.js
@@ -0,0 +1,16 @@
+
+var x = {};
+console.log(Number(x));
+console.log(String(x));
+console.log(Boolean(x));
+
+
+var xy = null;
+console.log(Number(xy));
+console.log(String(xy));
+console.log(Boolean(xy));
+
+var xyz = undefined;
+console.log(Number(xyz));
+console.log(String(xyz));
+console.log(Boolean(xyz));
